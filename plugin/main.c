956d4cbdfc84d69f99a4cff1cd18f28662b9a7e6
TheOfficialFloW <14246466+TheOfficialFloW@users.noreply.github.com>
2018-03-12 09:56:03 +0100

    Added support for 3.65/3.67

--- plugin/main.c
+++ plugin/main.c
@@ -147,7 +147,19 @@ int module_start(SceSize args, void *argp) {
 		return SCE_KERNEL_START_NO_RESIDENT;
 
 	// Get important function
-	module_get_offset(KERNEL_PID, info.modid, 0, 0x138C1, (uintptr_t *)&sceIoFindMountPoint);
+	switch (info.module_nid) {
+		case 0x9642948C: // 3.60 retail
+			module_get_offset(KERNEL_PID, info.modid, 0, 0x138C1, (uintptr_t *)&sceIoFindMountPoint);
+			break;
+
+		case 0xA96ACE9D: // 3.65 retail
+		case 0x3347A95F: // 3.67 retail
+			module_get_offset(KERNEL_PID, info.modid, 0, 0x182F5, (uintptr_t *)&sceIoFindMountPoint);
+			break;
+
+		default:
+			return SCE_KERNEL_START_NO_RESIDENT;
+	}
 
 	// Load SceUsbMass
 
